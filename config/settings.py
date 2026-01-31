647d66db340cec4568fdbed5ef79729a6368d29d
Lakshan Sivakumar <sivalakshan724@gmail.com>
2026-01-31 08:38:25 +0800

    Aws storage

--- config/settings.py
+++ config/settings.py
@@ -118,7 +118,7 @@ if 'AWS_ACCESS_KEY_ID' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_REGION_NAME = 'us-east-1' # Change to your Bucket region (e.g. ap-southeast-1)
+    AWS_S3_REGION_NAME = 'ap-southeast-2' # Change to your Bucket region (e.g. ap-southeast-1)
     AWS_S3_SIGNATURE_VERSION = 's3v4'
     AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = None
