55f5b57377890376b8b78688639412e84e2d9f27
Lakshan Sivakumar <sivalakshan724@gmail.com>
2026-03-08 02:11:06 +0800

    fixed the standard themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./core/templates/**/*.html",
-        "./core/views.py",
+        './core/templates/**/*.html',
+        './**/*.html'
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                theme: {
+                    page: 'var(--bg-page)',
+                    card: 'var(--bg-card)',
+                    secondary: 'var(--bg-secondary)',
+                    main: 'var(--text-main)',
+                    muted: 'var(--text-muted)',
+                    accent: 'var(--accent)'
+                }
+            }
+        }
     },
-    plugins: [
-        require('@tailwindcss/typography'),
-    ],
+    plugins: [],
 }
