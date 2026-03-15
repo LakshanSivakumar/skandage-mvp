44e64b530a71ffa4d35fc03583b1e70dd59f4db5
Lakshan Sivakumar <sivalakshan724@gmail.com>
2026-03-15 11:43:45 +0800

    fixed dark mode toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      './core/templates/**/*.html',
-      './**/*.html'
-    ],
-    theme: {
-      extend: {
-        colors: {
-          theme: {
-            page: 'var(--bg-page)',
-            card: 'var(--bg-card)',
-            secondary: 'var(--bg-secondary)',
-            main: 'var(--text-main)',
-            muted: 'var(--text-muted)',
-            accent: 'var(--accent)',
-            'hero-start': 'var(--hero-start)', // <-- Add this
-            'hero-end': 'var(--hero-end)'      // <-- Add this
-          }
+  darkMode: 'class', // <--- YOU MUST ADD THIS LINE
+  content: [
+    './core/templates/**/*.html',
+    './**/*.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        theme: {
+          page: 'var(--bg-page)',
+          card: 'var(--bg-card)',
+          secondary: 'var(--bg-secondary)',
+          main: 'var(--text-main)',
+          muted: 'var(--text-muted)',
+          accent: 'var(--accent)',
+          'hero-start': 'var(--hero-start)', 
+          'hero-end': 'var(--hero-end)'      
         }
       }
-    },
-    plugins: [],
-  }
+    }
+  },
+  plugins: [],
+}
